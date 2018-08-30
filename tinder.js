3eb849dfe9b1d7c664d3332b0a50628ed3861a7a
Mayank Jha <mayank25080562@gmail.com>
2018-08-31 01:12:09 +0800

    custom

--- tinder.js
+++ tinder.js
@@ -1,5 +1,15 @@
   a = setInterval(
     function(){
+          var ran := Math.random();
+          var time = ran*1000;
+          var dislike = Math.floor(ran*100);
+          var mod = 11;
+          var result = dislike%mod;
+          if (result==0) {
+            var elem = document.getElementsByClassName("recsGamepad__button--dislike");
+          elem[0].click();
+          } else {
           var elem = document.getElementsByClassName("recsGamepad__button--like");
-          elem[0].click()
-          },2000)
+          elem[0].click();
+          }
+          },time)
